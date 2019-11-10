c4a30f93392b7094e05515bcac5bf14d2ff6e3df
Semar Augusto <semaraugusto@Semar-Augustos-MacBook-Pro.local>
2019-11-10 12:04:01 -0300

    move method to charge from customer to rental

--- Customer.java
+++ Customer.java
@@ -13,27 +13,6 @@ class Customer {
 		return _name;
 	};
 
-	public double amountFor(Rental aRental){
-		double thisAmount = 0;
-		//determine amounts for each line
-		switch (aRental.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (aRental.getDaysRented() > 2)
-					thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += aRental.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (aRental.getDaysRented() > 3)
-					thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-				break;
-		}
-		return thisAmount;
-	}
-
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
@@ -41,7 +20,7 @@ class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
-			double thisAmount = amountFor(each);
+			double thisAmount = each.getCharge();
 
 			// add frequent renter points
 			frequentRenterPoints ++;
